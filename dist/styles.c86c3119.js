2da8cd48f1aac68992749a711f593351a29d4101
Marc <luxgroovemusic@gmail.com>
2021-09-17 23:11:46 -0400

    added hero

--- dist/styles.c86c3119.js
+++ dist/styles.c86c3119.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -144,12 +144,12 @@ function getBundleURL() {
 }
 
 function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
 }
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -184,12 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/styles.scss":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/styles.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\fonts\\PlayfairDisplay-Regular.ttf":[["PlayfairDisplay-Regular.ba2f3389.ttf","fonts/PlayfairDisplay-Regular.ttf"],"fonts/PlayfairDisplay-Regular.ttf"],"./..\\fonts\\PlayfairDisplay-Medium.ttf":[["PlayfairDisplay-Medium.b1dff077.ttf","fonts/PlayfairDisplay-Medium.ttf"],"fonts/PlayfairDisplay-Medium.ttf"],"./..\\fonts\\PlayfairDisplay-Bold.ttf":[["PlayfairDisplay-Bold.0caa2a8d.ttf","fonts/PlayfairDisplay-Bold.ttf"],"fonts/PlayfairDisplay-Bold.ttf"],"_css_loader":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../fonts/PlayfairDisplay-Regular.ttf":[["PlayfairDisplay-Regular.ba2f3389.ttf","fonts/PlayfairDisplay-Regular.ttf"],"fonts/PlayfairDisplay-Regular.ttf"],"./../fonts/PlayfairDisplay-Medium.ttf":[["PlayfairDisplay-Medium.b1dff077.ttf","fonts/PlayfairDisplay-Medium.ttf"],"fonts/PlayfairDisplay-Medium.ttf"],"./../fonts/PlayfairDisplay-Bold.ttf":[["PlayfairDisplay-Bold.0caa2a8d.ttf","fonts/PlayfairDisplay-Bold.ttf"],"fonts/PlayfairDisplay-Bold.ttf"],"./../fonts/Rubik-Light.ttf":[["Rubik-Light.e9917ee2.ttf","fonts/Rubik-Light.ttf"],"fonts/Rubik-Light.ttf"],"./../fonts/Rubik-Medium.ttf":[["Rubik-Medium.d1912f77.ttf","fonts/Rubik-Medium.ttf"],"fonts/Rubik-Medium.ttf"],"./../fonts/Rubik-Regular.ttf":[["Rubik-Regular.6d61f87c.ttf","fonts/Rubik-Regular.ttf"],"fonts/Rubik-Regular.ttf"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63784" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64502" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -248,9 +248,8 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
         assetsToAccept.forEach(function (v) {
           hmrAcceptRun(v[0], v[1]);
         });
-      } else if (location.reload) {
-        // `location` global exists in a web worker context but lacks `.reload()` function.
-        location.reload();
+      } else {
+        window.location.reload();
       }
     }
 
@@ -393,5 +392,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
 //# sourceMappingURL=/styles.c86c3119.js.map
